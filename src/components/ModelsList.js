85ff7dc60584995464ef46404daf8384fb44ade6
Monika Fac <monika.fac@gmail.com>
2019-02-11 16:01:15 +0100

    first part ready

--- src/components/ModelsList.js
+++ src/components/ModelsList.js
@@ -4,8 +4,10 @@ import './ModelList.css'
 const ModelsList = (props) => {
     const users=props.users.map(user=>(
         <div key={user.login.uuid}>
+        <div  className="userList">
         <img src={user.picture.large} alt={user.name.last}/>
         <p>{`${user.name.first} ${user.name.last}`}</p>
+        </div>
         </div>
     ))
     return ( <div className='users'>
